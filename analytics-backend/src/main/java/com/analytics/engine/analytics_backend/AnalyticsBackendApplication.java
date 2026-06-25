b6e311d13f20dbef545e8764637e0120541a5b21
YATRAONLINE\v-gitansh.manchanda <v-gitansh.manchanda@yatra.com>
2026-06-25 15:32:04 +0530

    feat: Init java spring boot project for analytics backend

---
+++ analytics-backend/src/main/java/com/analytics/engine/analytics_backend/AnalyticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.analytics.engine.analytics_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsBackendApplication.class, args);
+	}
+
+}
